659fdb396afd2e839c984d64c7da17adedc3d08d
yukizarashi001 <yukizarashi.w001@gmail.com>
2020-09-15 01:01:43 +0900

    Chapter1-2

---
+++ Chapter1/Chapter1$.java
@@ -0,0 +1,15 @@
+object Chapter1 extends scala.AnyRef with scala.App {
+  val name : _root_.scala.Predef.String = { /* compiled code */ }
+  val id : scala.Int = { /* compiled code */ }
+  lazy val lazyDate : java.util.Date = { /* compiled code */ }
+  lazy val lazyDate2 : java.util.Date = { /* compiled code */ }
+  def echo(str : _root_.scala.Predef.String) : scala.Unit = { /* compiled code */ }
+  def double(n : scala.Int) : scala.Int = { /* compiled code */ }
+  val printer : `<empty>`.Printer = { /* compiled code */ }
+  def isAlphameric(str : _root_.scala.Predef.String) : scala.Boolean = { /* compiled code */ }
+  val isAlphamericF : scala.Function1[_root_.scala.Predef.String, scala.Boolean] = { /* compiled code */ }
+  val isAlphamericF2 : scala.Function1[_root_.scala.Predef.String, scala.Boolean] = { /* compiled code */ }
+  val isAlphamericF3 : scala.AnyRef with scala.Function1[_root_.scala.Predef.String, scala.Boolean] = { /* compiled code */ }
+  val words : scala.collection.immutable.Seq[java.lang.String] = { /* compiled code */ }
+  val alphamericWords : scala.collection.immutable.Seq[java.lang.String] = { /* compiled code */ }
+}
