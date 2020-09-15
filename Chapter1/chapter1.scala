6c25bdcb62a20d1ef76facb49a7c6e1b7ff99b06
yukizarashi001 <yukizarashi.w001@gmail.com>
2020-09-16 02:01:09 +0900

    Chapter1-6

--- Chapter1/chapter1.scala
+++ Chapter1/chapter1.scala
@@ -58,6 +58,115 @@ object Chapter1 extends App {
   println("words:" + alphamericWords)
 
   println("------------------------------------------")
+
+  println("## if/else")
+  val weight = 120
+  val message = if(weight <= 100) {
+    "OK"
+  } else {
+    "NG"
+  }
+  println(message)
+  // 三項演算子の代わりにif/elseで代入前に判定が可能
+  val message2 = if(weight <= 100) "OK" else "NG"
+  println("三項演算子:" + message2)
+  println("------------------------------------------")
+
+  println("## match")
+  // switch文のような使い方
+  val n = 5
+  n match {
+    case 1 => println("Gold")
+    case 2 => println("Silver")
+    case 3 => println("Bronze")
+    case other => println("You don't get a prize")
+  }
+  // Option型の判定
+  // Some型の場合
+  val maybeNum: Option[Int] = Some(123)
+  val num: Int = maybeNum match {
+    case Some(num) => num
+    case None => 0
+  }
+  println(num)
+  // None型の場合
+  val maybeNum2: Option[Int] = None
+  val num2: Int = maybeNum2 match {
+    case Some(num2) => num2
+    case None => 0
+  }
+  println(num2)
+  // 値がマイナスの時0にしたい
+  val maybeNum3: Option[Int] = Some(-1)
+  val num3 = maybeNum3 match {
+    case Some(num3) if num3 < 0 => 0
+    case Some(num3) => num3
+    case None => 0
+  }
+  println(num3)
+  // case条件を複数にする
+  val num4 = 3
+  num4 match {
+    case 1 | 2 | 3 => println("Less than 4")
+    case 4 => println("Equal to 4")
+    case other => println("Greater than 4")
+  }
+  println("------------------------------------------")
+
+  println("## ループ")
+  // Scalaではwhileは基本使わない.
+  // filter(抽出)
+  // 素直に書いた場合
+  val filtered1 = Seq(1,2,3).filter(i => i > 1)
+  println("filtered1: " + filtered1)
+
+  // 省略記法(i => iなど引数と関数に利用する値が同じ時)
+  val filtered2 = Seq(1,2,3).filter(_ > 1)
+  println("filtered2: " + filtered2)
+
+  // map(変換)
+  // 素直に書いた場合。
+  val mapped1 = Seq(1,2,3).map(i => i * 2)
+  println("mapped1: " + mapped1)
+
+  // 省略記法
+  val mapped2 = Seq(1,2,3).map(_ * 2)
+  println("mapped2: " + mapped2)
+
+  // コレクションメソッドの内容の複数行記述
+  val mapped3 = Seq(1,2,3).map { i =>
+    println(i)
+    i * 2
+  }
+
+  // flatMap(コレクション内コレクション処理後、全体結合）
+  val flatMapped = Seq(Seq(1,2), Seq(3,4)).flatMap {s => s.filter(_ > 1)}
+  println(flatMapped)
+  val flatMapped2 = (1 to 3).flatMap(i => (2 to 4).map(j => i * j).filter(_ % 2 == 0))
+  println(flatMapped2)
+
+  // flatMapでコレクションを操作・結合する場合。
+  val results: Seq[Int] = (1 to 3).flatMap { i =>
+    (2 to 4).flatMap { j =>
+      (3 to 5).map(k => i * j * k).filter(_ % 3 == 0)
+    }
+  }
+  println(results)
+
+  // forでコレクションを操作・結合する場合（仕組みは後々）。
+  val results2: Seq[Int] = for {
+    i <- (1 to 3)
+    j <- (2 to 4)
+    k <- (3 to 5)
+    result = (i * j * k) if result % 3 == 0
+  } yield result
+  println(results2)
+  println("------------------------------------------")
+
+  // memo: scalacはJavaで作られているScalaのコンパイラを行うプログラムを実行するコマンド。
+  // sbt, Gradle, Mavenでもコンパイルできる仕組みはある。
+
+
 }
 
 class Printer {
